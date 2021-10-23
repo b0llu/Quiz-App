f23539c43e6ebba5f17dd44afa160691a6822b4f
Dhruv <samantdhruv@gmail.com>
2021-10-23 09:27:38 +0530

    added questions for the quiz

---
+++ app.js
@@ -0,0 +1,59 @@
+const quizData = [
+    {
+        question: 'What is my name?',
+        a: 'Dhru',
+        b: 'Dhruv',
+        c: 'Dhruva',
+        d: 'None of Above',
+        correct: 'b'
+    },{
+        question: 'What is my Age?',
+        a: '19',
+        b: '21',
+        c: '22',
+        d: '20',
+        correct: 'd'
+    }, {
+        question: 'Where do I Live?',
+        a: 'Banglore',
+        b: 'Mumbai',
+        c: 'Ratnagiri',
+        d: 'Saudi Arabia',
+        correct: 'c'
+    }, {
+        question: 'What is my Favourite Sport?',
+        a: 'Cricket',
+        b: 'Football',
+        c: 'Rugby',
+        d: 'Kabaddi',
+        correct: 'b'
+    }, {
+        question: 'What is my Height?',
+        a: "5'7",
+        b: "6'1",
+        c: "4",
+        d: "5'11",
+        correct: 'd'
+    }, {
+        question: 'Know my Favourite Anime?',
+        a: 'Attack On Titan',
+        b: 'Dragon Ball',
+        c: "Stein's Gate",
+        d: 'Black Clover',
+        correct: 'c'
+    }, {
+        question: 'Know my Favourite Series?',
+        a: 'Breaking Bad',
+        b: 'Succession',
+        c: 'Game of Thrones',
+        d: 'Dark',
+        correct: 'b'
+    }, {
+        question: 'Know my Favourite Politician?',
+        a: 'Amit Shaha',
+        b: 'Modi',
+        c: 'Rahul Gandhi',
+        d: 'Arvin Kejriwal',
+        correct: 'b'
+    }
+]
